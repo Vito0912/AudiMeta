ff27d22e1c56951631eccc409a87eb5de1afc577
Vito0912 <86927734+Vito0912@users.noreply.github.com>
2025-04-13 09:28:37 +0200

    init

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-npx prisma db push --skip-generate
-
-exec "$@"
--- instrument.js
+++
@@ -1,9 +0,0 @@
-// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
-const Sentry = require('@sentry/node');
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-
-  // Set sampling rate for profiling - this is evaluated only once per SDK.init
-  profileSessionSampleRate: 1.0,
-});
