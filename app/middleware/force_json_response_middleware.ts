e907a695b73001b17d7a6aa7e50927a89dd04349
Vito0912 <86927734+Vito0912@users.noreply.github.com>
2025-08-06 21:21:45 +0200

    Reapply "Refactor route params and OpenAPI decorators"
    This reverts commit 85e928a1e581f12a9016ee05432efa6c0dcfd9e2.

--- app/middleware/force_json_response_middleware.ts
+++ app/middleware/force_json_response_middleware.ts
@@ -7,11 +7,44 @@ import type { NextFn } from '@adonisjs/core/types/http'
  * validator errors or auth errors to return a JSON response.
  */
 export default class ForceJsonResponseMiddleware {
-  async handle({ request }: HttpContext, next: NextFn) {
+  async handle({ request, response }: HttpContext, next: NextFn) {
     const headers = request.headers()
+
+    if (
+      request.completeUrl(false).endsWith('/api-docs') &&
+      headers.accept &&
+      headers.accept.includes('application/json')
+    ) {
+      const nextContext = await next()
+
+      try {
+        const body = response.getBody()
+
+        let parsedBody: any
+        if (typeof body === 'string') {
+          parsedBody = JSON.parse(body)
+        } else {
+          parsedBody = body
+        }
+
+        const modifiedBodyString = JSON.stringify(parsedBody)
+          .replace(/:asin/g, '{asin}')
+          .replace(/:sku/g, '{sku}')
+
+        response.send(modifiedBodyString)
+      } catch (error) {
+        console.error('Failed to modify response body:', error)
+        response.send('{"error": "Failed to modify response body"}')
+        response.status(400)
+      }
+
+      return nextContext
+    }
+
     if (request.completeUrl(false).endsWith('/api-docs')) {
       return next()
     }
+
     headers.accept = 'application/json'
 
     return next()
