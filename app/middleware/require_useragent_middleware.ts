a09cc8e2aaffcbd8fcc6e50d37b8184fca668f5b
Vito0912 <86927734+Vito0912@users.noreply.github.com>
2025-08-15 16:37:27 +0200

    Added new description to stop current user-agent

--- app/middleware/require_useragent_middleware.ts
+++ app/middleware/require_useragent_middleware.ts
@@ -25,7 +25,8 @@ export default class RequireUseragentMiddleware {
     if (RequireUseragentMiddleware.blacklist.some((ua) => userAgent.startsWith(ua))) {
       ctx.logger.warn(`Request with blacklisted User-Agent: ${userAgent}`)
       ctx.response.status(403).send({
-        error: 'This User-Agent is blacklisted. Please open an issue to resolve this.',
+        error:
+          'This User-Agent is blacklisted. If you used a generic User-Agent, this is likely because this generic user agent has been used to make a lot of request. To use this service for more than testing, please provide a meaningful User-Agent. This must include a way to contact you and identify the project. Please see https://www.rfc-editor.org/rfc/rfc9110#name-user-agent for guidance on creating a proper User-Agent. If you are already using a non-generic header, please open an issue on GitHub at https://github.com/Vito0912/AudiMeta/ to resolve this. This service is provided for free. In exchange I need to protect it from abuse and want to know why that much request are needed. I will block those very early.',
       })
       return
     }
